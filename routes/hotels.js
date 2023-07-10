6bb1b8dcc4ca2e73ff21fbd2316b6a299c9d474c
박건태 <qkrrjsxo456@naver.com>
2023-07-10 18:09:49 +0900

    login,user,admin authentication, authorization, verifytoken(jwt) 완료

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
 //Create
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
 //update
-router.put('/:id', updateHotel)
+router.put('/:id', verifyAdmin, updateHotel)
 //delete
-router.delete('/:id', deleteHotel)
+router.delete('/:id', verifyAdmin, deleteHotel)
 //Get
 router.get('/:id', getHotel)
 //Get all
